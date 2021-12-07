6869506b4037db2766c9edc6ab62cc7cf31decc9
ILikeMonday <worldrocker64@gmail.com>
2021-12-07 17:14:57 +0700

    lab9 done

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,11 +1,40 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your property: "
-"Your status = "
-"UNFRIEND"
-"FRIEND"
-"BEST FRIEND"
-"ONE-NIGHT-STAND"
-"MARRIED"
-*/
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num;
+    int height;
+    int property;
+    cout << "Enter your age: ";
+    cin >> num;
+    if (num <= 20){
+        cout << "Enter your height: ";
+        cin >> height;
+    if (height < 160)
+        cout << "Your status = " << "UNFRIEND";
+        else if (height < 175)
+        cout << "Your status = " << "FRIEND";
+        else
+            cout << "Enter your property: ";
+            cin >> property;
+            if (property > 69000000)
+            cout << "Your status = " << "MARRIED";
+            else
+                cout << "Your status = " << "ONE-NIGHT-STAND";
+    }else{
+        if (num < 30){
+            cout << "Enter your property: ";
+            cin >> property;
+            if (property > 10000000)
+            cout << "Your status = " << "BEST FRIEND";
+            else
+                cout << "Your status = " << "UNFRIEND";  
+            }
+            else{
+            if (num > 30){
+            cout << "Your status = " << "UNFRIEND";
+            }
+            }
+    }       
+}        
+
